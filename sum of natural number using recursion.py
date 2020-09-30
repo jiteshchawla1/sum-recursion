06e1caa938a158759a8474b90c4ceca36f9a3bc2
jiteshchawla1 <jiteshchawla1511@gmail.com>
2020-09-30 21:08:58 +0530

    Add files via upload

---
+++ sum of natural number using recursion.py
@@ -0,0 +1,11 @@
+def recur_sum(n):
+     if n<=1:
+          return n:
+     else:
+          return n+recur_sum(n-1)
+num=int(input("enter a number "))
+if num<0:
+     print("enter a +ve number")
+else:
+     print("enter a number ", recur_sum(num))
+     
